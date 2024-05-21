25d8f3e6b24925f90a812b0255e5c810433f0829
김도현 <returner@com2us.com>
2024-05-21 15:47:21 +0900

    silo dash 구동 완료

--- Aspire/Aerifloat.AppHost/Program.cs
+++ Aspire/Aerifloat.AppHost/Program.cs
@@ -1,10 +1,9 @@
 
-using Microsoft.Extensions.Options;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Aerifloat_Silo>("silo");
-    //.WithHttpEndpoint(port:8123, isProxied:false)
+builder.AddProject<Projects.Aerifloat_Silo>("silo")
+    .WithHttpEndpoint(port: 8123, isProxied: false);
     //.WithReplicas(10);
 
 builder.AddProject<Projects.Aerifloat_Api_Backend>("api-backend");
