b7675c3c75c5c70fed1af852e01b41c658ca83c3
김도현 <returner@com2us.com>
2024-05-28 10:10:34 +0900

    GuidToLongExtension

---
+++ Aerifloat.Api.Common/Extensions/GuidToLongExtension.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography;
+
+namespace Aerifloat.Api.Common.Extensions
+{
+    public static class GuidToLongExtension
+    {
+        public static long ToLong(this Guid guid)
+        {
+            byte[] bytes = SHA256.HashData(guid.ToByteArray());
+            var hashdata = BitConverter.ToInt64(bytes, 0);
+            return hashdata < 0 ? hashdata * -1 : hashdata;
+        }
+    }
+}
